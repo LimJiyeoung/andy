cfc9d16e92fcc56bcb1fcb4bed9fb4a3bb64a7ce
Liamkim <liamkim1018@gmail.com>
2021-12-04 17:00:14 +0900

    Initial commit

---
+++ src/test/java/com/toystory/andy/AndyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toystory.andy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
